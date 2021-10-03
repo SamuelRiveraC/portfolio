6208fad2f04206c24051aee0efcdf1dddaaf532c
samuelriverac <samuelrivera64@gmail.com>
2021-10-03 18:04:30 -0400

    change title

--- components/Hero.js
+++ components/Hero.js
@@ -18,7 +18,7 @@ export default class Hero extends React.Component {
       <div className="row hero pb-5 pb-md-0">
 
         <div className="col-12 col-md-6 text-center text-md-left order-2 order-md-1">
-          <h1> Front End Engineer & Web developer </h1>
+          <h1> Software Engineer & Web developer </h1>
           <p>
             Hi, thanks for checking out my profile! I’m Samuel Rivera, a Front end engineer and web developer with more than 3 years of experience developing purposeful, consistent and attractive interfaces for business, startups and tech companies to appeal to clients and maintain their interest
           </p>
