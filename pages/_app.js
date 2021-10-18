5d190546bb4a30409752d1475f8bd816636ca2b6
samuelriverac <samuelrivera64@gmail.com>
2021-10-18 12:25:24 -0400

    Added a small loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import '../styles/index.scss'
+import Router from 'next/router';
+
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());  
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+
+
+
